824a3832dec815f39852150ffe589d41af43611e
Tony Eder <tonyeder11@gmail.com>
2024-02-01 12:23:03 -0800

    first test passed

--- lib/looping.py
+++ lib/looping.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    # using a loop countdown from 10
+    # when loop gets to 0 stop countdown
+    # when countdown stop print('happy new year')
+        i = 10
+        while i >= 0:
+            print("Happy New Year!") if i < 1 else print(i)
+            i -= 1
+    
 
 def square_integers(int_list):
     # code goes here!
